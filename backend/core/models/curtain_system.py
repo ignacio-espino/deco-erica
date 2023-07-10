f71b2da23b11f90ff1992286f3b4a047051e9e67
iespino <iespino@eryx.co>
2023-07-09 21:42:39 -0300

    Add CurtainSystem model

---
+++ backend/core/models/curtain_system.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class CurtainSystem(models.Model):
+    _name = models.CharField('Nombre', max_length=70, null=True, blank=True)
+    _price = models.DecimalField('Precio', max_digits=10, decimal_places=2)
+
+    @classmethod
+    def new_from(cls, name, price):
+        return cls(_name=name, _price=price)
+
+    def __str__(self):
+        return f'{self.name()}'
+
+    def name(self):
+        return self._name
+
+    def price(self):
+        return self._price
+
+    class Meta:
+        verbose_name = 'Sistema de cortina'
+        verbose_name_plural = 'Sistemas de cortinas'
