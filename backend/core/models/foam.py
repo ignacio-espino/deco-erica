a8cf4d140b052019686f57f38cd2759ce98ea019
iespino <iespino@eryx.co>
2023-07-09 21:41:55 -0300

    Add Foam model

---
+++ backend/core/models/foam.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Foam(models.Model):
+    _type = models.CharField('Tipo', max_length=70, null=True, blank=True)
+    _price = models.DecimalField('Precio', max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.type()}'
+
+    @classmethod
+    def new_from(cls, type, price):
+        return cls(_type=type, _price=price)
+
+    def type(self):
+        return self._type
+
+    def price(self):
+        return self._price
+
+    class Meta:
+        verbose_name = 'Espuma'
+        verbose_name_plural = 'Espumas'
