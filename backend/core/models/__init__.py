17499788fcb2c663af43e77db8be23130cd67c55
iespino <iespino@eryx.co>
2023-07-09 21:46:24 -0300

    Add models imports in init file

--- backend/core/models/__init__.py
+++ backend/core/models/__init__.py
@@ -1,1 +1,9 @@
 import core.models.customer
+import core.models.fabric
+import core.models.curtain_system
+import core.models.sewing_method
+import core.models.foam
+import core.models.quote
+import core.models.quote_entry
+import core.models.curtain_quote_entry
+import core.models.upholster_quote_entry
