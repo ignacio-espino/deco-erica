0e85c9d1737f97b8374195914fa9c5c071e37668
iespino <iespino@eryx.co>
2023-10-21 02:47:21 -0300

    Change convert quotation to sale (add dates)

--- backend/core/models/decoerica.py
+++ backend/core/models/decoerica.py
@@ -1,3 +1,5 @@
+import datetime
+
 from core.models.sale import Sale
 
 
@@ -5,6 +7,7 @@ class DecoErica:
 
     def convert_to_sale(self, quotation):
         sale_number = Sale.objects.count() + 1
-        sale = Sale(_quote=quotation, _number=sale_number)
+        delivery_date = datetime.datetime.today() - datetime.timedelta(days=15)
+        sale = Sale(_quote=quotation, _number=sale_number, _delivery_date=delivery_date)
         sale.save()
         return sale
