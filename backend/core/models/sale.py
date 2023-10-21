5b509b349a26fd0b2921e9e3a1a206fabed142e4
iespino <iespino@eryx.co>
2023-10-21 02:46:11 -0300

    Add date and delivery_date to Sale + migration

--- backend/core/models/sale.py
+++ backend/core/models/sale.py
@@ -5,6 +5,8 @@ class Sale(models.Model):
     _number = models.IntegerField('Número', blank=True, null=True)
     _quote = models.OneToOneField('Quote', on_delete=models.CASCADE, blank=True, null=True, related_name='sale',
                                   verbose_name='Cotización')
+    _delivery_date = models.DateTimeField('Fecha de entrega', blank=True, null=True)
+    _date = models.DateTimeField('Fecha', auto_now_add=True)
 
     def __str__(self):
         return f'Venta {self.number()}'
