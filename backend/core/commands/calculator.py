e7aac2e44e325a4eaf32a2ced26ed42a6c9df7de
iespino <iespino@eryx.co>
2024-03-03 23:32:58 -0300

    Calculate taylor price

--- backend/core/commands/calculator.py
+++ backend/core/commands/calculator.py
@@ -1,6 +1,9 @@
+from decimal import Decimal
 from typing import List
 
 from core.commands.base import Command, Validator
+from core.models.fabric import Fabric
+from core.models.sewing_method import SewingMethod
 
 
 class CalculatorCommand(Command):
@@ -12,6 +15,7 @@ class CalculatorCommand(Command):
 
     def _execute_from_successful_validation(self, result):
         # TODO Adaptar con las cuentas necesarias
+        calculator = Calculator()
         new_data = []
         fabric_total_cost = 0
         sewing_total_cost = 0
@@ -26,7 +30,7 @@ class CalculatorCommand(Command):
             entry = self._data['entries'][index]
             data_entry = entry
             data_entry['systemPrice'] = entry['systemPrice'] + 777
-            data_entry['taylorPrice'] = entry['taylorPrice'] + 777
+            data_entry['taylorPrice'] = calculator.calculate_taylor_price(entry)
             data_entry['sewingPrice'] = entry['sewingPrice'] + 777
             data_entry['subtotal'] = entry['subtotal'] + 777
             data_entry['curtainTotal'] = entry['curtainTotal'] + 777
@@ -60,3 +64,17 @@ class CalculatorCommand(Command):
         }
         new_data.append({"totals": totals})
         result.set_object(new_data)
+
+
+class Calculator:
+
+    def calculate_taylor_price(self, entry):
+        # (Ancho x Tipo de confeccion +0.20cm) x $ de tela
+        fabric_width = entry['width']
+        sewing_method_name = entry['sewing']
+        sewing_method = SewingMethod.objects.get(_name=sewing_method_name)
+        sewing_value = sewing_method.value()
+        fabric_code = entry['product'][0]
+        fabric = Fabric.objects.get(_code=fabric_code)
+        fabric_price = fabric.price()
+        return ((fabric_width * sewing_value) + Decimal(0.2)) * fabric_price
